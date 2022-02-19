0b64dd4cd27cf77d562c9b88a5645a2db027d98c
Savin-Park <savinpark0315@gamil.com>
2022-02-19 18:16:28 +0900

    Todo Application 생성 + 새로운 todo 등록 기능

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import React, { Component, useEffect, useState } from 'react';
 import './App.css';
 
+// components
+import List from './components/List.jsx';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  // ---- useState ---- //
+  // 등록한 todo들을 담은 배열
+  const [todos, setTodos] = useState(['Java 공부하기', 'React 공부하기', '운동하기']);
+  // 새로운 todo
+  const [newTodo, setNewTodo] = useState();
+
+
+  // changeInputData : newTodo에 input에 입력한 내용을 저장하는 함수
+  const changeInputData = (e) => {
+    setNewTodo(e.target.value);
+  }
+  // addTodo
+  const addTodo = (e) => {
+    e.preventDefault(); // 기본값 form 전송방지
+    setTodos([...todos, newTodo]);
+  }
+
+  // ---- useEffect ---- //
+  useEffect(() => {
+    console.log('새롭게 렌더링 되었습니다.');
+  // });
+  }, [todos]); // todos에 변경사항이 생기면 콜백함수 실행
+
+  return(
+    <>
+      <h1>Todo Application</h1>
+
+      <form action="">
+        <input type="text" name="" onChange={changeInputData}/>
+        <button onClick={addTodo}>ADD</button>
+      </form>
+
+      <List todos={todos} />
+    </>
   );
 }
 
---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function List({todos}) {
+
+  const todoList = todos.map(todo => <li>{todo}</li>);
+  
+  return(
+    <ul>
+      {todoList}
+    </ul>
+  );
+}
+
+export default List;
