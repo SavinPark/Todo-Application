fae7cfc4e8ef2db748e050dd88db6680c10bce26
Savin-Park <savinpark0315@gamil.com>
2022-02-20 16:51:47 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
-// custom Hook
+// Custom Hook
 import useFetch from './useFetch.js';
 
-// components 
+// Components 
 import List from './components/List.jsx';
 
 
@@ -12,20 +12,16 @@ import List from './components/List.jsx';
 // App Component
 function App() {
 
-  // ---- useState ---- //
-  // 등록한 todo들을 담은 배열
-  const [todos, setTodos] = useState(['Java 공부하기', 'React 공부하기', '운동하기']);
-  // 새로운 todo
-  const [newTodo, setNewTodo] = useState();
+  const [todos, setTodos] = useState([]);  // todos
+  const [newTodo, setNewTodo] = useState();  // new todos
   
   const loading = useFetch(setTodos, 'http://localhost:4000/initialtodos');
 
-  // changeInputData : newTodo에 input에 입력한 내용을 저장하는 함수
-  const changeInputData = (e) => {
+  const changeInputData = (e) => { // changeInputData : newTodo에 input에 입력한 내용을 저장하는 함수
     setNewTodo(e.target.value);
   }
-  // addTodo : 새로운 todo를 배열에 추가하는 함수
-  const addTodo = (e) => {
+  
+  const addTodo = (e) => { // addTodo : 새로운 todo를 배열에 추가하는 함수
     e.preventDefault(); // 기본값 form 전송방지
     setTodos([...todos, {'title': newTodo, 'todoCode': todos.length, 'contents': '', done: false, edit: false}]);
   }
